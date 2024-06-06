33cd5bd23a72ae5f6fcba4b0eab95e747524ac99
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-06-06 22:55:06 +0200

    commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,7 +15,8 @@ module.exports = {
       "ref": "origin/main",
       "repo": "git@github.com:jarle/matstack-dev-landing.git",
       "path": "/root/deployments/matstack-dev-landing",
-      "post-deploy": "npm install"
+      'post-deploy':
+        'npm install && npm run build && pm2 reload ecosystem.config.cjs --env production',
     }
   }
 }
