bd8755378bcfdea60acf79b193cdd9b6feb60fd8
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-06-23 21:27:34 +0200

    commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,20 +7,8 @@ module.exports = {
       kill_timeout: 810000,
       cron_restart: '10 */6 * * *',
       env: {
-        PORT: 42402,
         NODE_ENV: 'production',
       }
     },
   ],
-  deploy: {
-    production: {
-      "user": "root",
-      "host": ["165.227.208.86"],
-      "ref": "origin/main",
-      "repo": "git@github.com:jarle/matstack-dev-landing.git",
-      "path": "/root/deployments/matstack-dev-landing",
-      'post-deploy':
-        'source ~/.bashrc && npm install && npm run build && pm2 reload ecosystem.config.cjs --env production',
-    }
-  }
 }
