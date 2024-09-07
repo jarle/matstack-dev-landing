bd356b9ddda46beef66be51b0fcc278205852517
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-09-07 12:26:22 +0200

    commit

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,11 @@
 
 import router from '@adonisjs/core/services/router'
 
+router.any('*', async ({ response }) => {
+  response.redirect('https://matstack.dev/remix-adonisjs')
+})
+  .domain('remix-adonisjs.matstack.dev')
+
 router.any('*', async ({ remixHandler }) => {
   return remixHandler()
 })
